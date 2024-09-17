3fad8e4adc13b90ccd8175aec23736c2ed3da970
Lorena <lorenarojasarango2@gmail.com>
2024-09-17 08:01:19 -0500

    correcion
    uwu

--- UnityyJson2D/Assets/_Scrits/PlayerControl.cs
+++ UnityyJson2D/Assets/_Scrits/PlayerControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControl: MonoBehaviour
 {
     
     [ Header("Velocidad y salto") ] 
